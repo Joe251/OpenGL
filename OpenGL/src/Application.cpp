2c965d245b6904c698ce739f95b8bfdcf99823c0
Joe251 <940621557@qq.com>
2019-01-29 12:48:04 +0800

    Draw square with index buffer

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -110,13 +110,16 @@ int main(void)
 
 	float positions[] = {
 		-0.5f, -0.5f,
-		 0.5f,  -0.5f,
-		 0.5f, 0.5f,
-
-		 0.5f, 0.5f,
-		 -0.5f, 0.5f,
-		 -0.5f, -0.5f
+		 0.5f, -0.5f,
+		 0.5f,  0.5f,
+		-0.5f,  0.5f
 	};
+
+	unsigned int indices[] = {
+		0, 1, 2,
+		2, 3, 0
+	};
+
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
@@ -124,6 +127,11 @@ int main(void)
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+	unsigned int ibo;
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
 	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
 	std::cout << "SOURCE" << std::endl;
@@ -139,7 +147,8 @@ int main(void)
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 		
 		glEnd();
 
